2c8cbbce1da2ae9d4e27509ee3271e8636218541
sweetkeys <shaun@shaunsweet.com>
2016-05-19 11:28:04 -0700

    moved repo over

---
+++ jquery.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	var newGame = new ConnectFourGame();
+	var lock = false;
+	// console.log("heyyyy");
+
+	var connection = new WebSocket('ws:172.16.51.254:8001');
+        connection.onmessage = function(event){
+        	console.log(event.data);
+        	receivedMessage = JSON.parse(event.data);
+        	console.log(receivedMessage);
+        	// console.log(receivedMessage.column);
+
+        	if (receivedMessage.column){
+                  if (!newGame.checkWin()){
+                     var column = receivedMessage.column;
+        							// var column = $(this).attr('class').slice(-1);
+        							var cell = newGame.playTurn(Number(column))
+        							// connection.send(column);
+        							var row = Math.floor(cell/9)
+        							$('#row'+row + ' .col' + column).css('background-color', newGame.playerColor )
+        							if (newGame.checkWin()){
+        								$("#winner-status").html(newGame.playerColor.toUpperCase() + " WINS");
+        							}
+
+        						}
+        						lock = false;
+        					}
+
+                                               if (receivedMessage.lock){
+                                                      lock = true;
+                                              }
+
+                                              if (receivedMessage.refresh) {
+                                                alert("Your opponent left. :( New game initiated.");
+                                                location.reload();
+                                        }
+
+
+                                }
+
+                                $('.board-container div div').on('click', function(){
+                                     if (!lock){
+                                          var column = $(this).attr('class').slice(-1);
+                                          jsonObject = JSON.stringify({column: column});
+                                          connection.send(jsonObject);}
+                                  }
+                                  )
+
+                                $('#new-game-button').click(function() {
+                                        location.reload();
+                                });
+                        })
---
+++ server.js
@@ -0,0 +1,30 @@
+var ws = require("nodejs-websocket")
+
+var clients = []
+var messages = []
+
+var server = ws.createServer(function(conn){
+    clients.push(conn);
+    
+    console.log("new connection");
+
+    conn.on("text", function(receivedMessage){
+        var column = receivedMessage
+    	
+        console.log("recieved")
+        clients.forEach(function(client){
+            client.sendText(column); 
+
+        })
+        conn.sendText(JSON.stringify({lock: true}));
+
+    })
+    conn.on("close", function(code, reason){
+        clients.splice(clients.indexOf(conn), 1);
+        clients.forEach(function(client){
+            client.sendText(JSON.stringify({refresh: true}))
+        })
+        console.log("connection closed for "+reason)
+    })
+}).listen(8001)
+
